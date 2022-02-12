419f78d8a190fed9df46a48fb160f566f2a524dd
Jenniffer <priscilaavila71@gmail.com>
2022-02-12 20:05:01 +0100

    build maven project

---
+++ Saluda2/src/main/java/com/mgh/edd/App.java
@@ -0,0 +1,13 @@
+package Saluda2.src.main.java.com.mgh.edd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
